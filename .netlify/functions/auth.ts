2675e654ef48e6deee9d00fbe2ca95ea871f35c8
Vladimir V.M <vvmyagdeev@gmail.com>
2024-06-17 03:33:30 +0400

    feat<auth>: validation

--- .netlify/functions/auth.ts
+++ .netlify/functions/auth.ts
@@ -1,5 +1,8 @@
+import crypto from 'crypto';
 import { Handler } from "@netlify/functions";
 import { sendAdminMessage } from "./webhook";
+
+const { TELEGRAM_BOT_TOKEN } = process.env;
 
 export const handler: Handler = async (event) => {
     const {
@@ -7,11 +10,40 @@ export const handler: Handler = async (event) => {
         rawQuery,
         queryStringParameters
     } = event;
+    
+    const data_check_string = rawQuery.split('&hash=')[0];
+    // secret_key = HMAC_SHA256(<bot_token>, "WebAppData")
+    // if (hex(HMAC_SHA256(data_check_string, secret_key)) == hash) {
+    // // data is from Telegram
+    // }
+    
+    // i need some hmac sha256 library
+    const secretKey = crypto.createHmac('sha256', TELEGRAM_BOT_TOKEN as string)
+                          .update('WebAppData')
+                          .digest();
+
+    const computedHash = crypto.createHmac('sha256', secretKey)
+                                .update(data_check_string)
+                                .digest('hex');
+
+    const hash = queryStringParameters?.hash;
+
+    if (computedHash !== hash) {
+        return {
+            statusCode: 400,
+            body: "Invalid hash",
+        };
+    }
+
     await sendAdminMessage({
         rawUrl,
         rawQuery,
-        queryStringParameters
+        queryStringParameters,
+        data_check_string,
+        hash,
+        computedHash,
     });
+
     return {
         statusCode: 200,
         body: "Webhook received",
